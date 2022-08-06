008df314bd2da182470c3ab4e724b6a5a3c9a05a
sgash708 <sugash7081023@gmail.com>
2022-08-06 16:11:55 +0900

    docs: 各ファイルについて説明

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # gql
+
+## 構造
+
+```:bash
+.
+├── README.md
+├── go.mod
+├── go.sum
+├── gqlgen.yml               // 設定ファイル
+├── graph
+│   ├── generated            // さわらない
+│   │   └── generated.go
+│   ├── model                // graphqlのモデル
+│   │   └── models_gen.go
+│   ├── resolver.go          // 型を定義する
+│   ├── schema.graphqls      // graphqlのスキーマ定義
+│   └── schema.resolvers.go  // schema.graphqlsで生成されたresoloverの実装
+└── server.go                // エントリポイント
+
+3 directories, 10 files
+```
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -22,10 +22,18 @@ func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
 }
 
 // Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+func (r *Resolver) Mutation() generated.MutationResolver {
+	return &mutationResolver{r}
+}
 
 // Query returns generated.QueryResolver implementation.
-func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+func (r *Resolver) Query() generated.QueryResolver {
+	return &queryResolver{r}
+}
 
-type mutationResolver struct{ *Resolver }
-type queryResolver struct{ *Resolver }
+type mutationResolver struct {
+	*Resolver
+}
+type queryResolver struct {
+	*Resolver
+}
